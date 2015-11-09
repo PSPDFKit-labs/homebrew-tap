b08960ef4aee99ed637c63a5b1d1bc528ee73cef
Julian Grosshauser <julian@juliangrosshauser.com>
2015-11-09 19:15:02 +0100

    Update to version 0.2.7.pspdfkit2

--- xctool.rb
+++ xctool.rb
@@ -1,9 +1,9 @@
 class Xctool < Formula
   desc "Drop-in replacement for xcodebuild with a few extra features"
   homepage "https://github.com/PSPDFKit-labs/xctool"
-  url "https://github.com/PSPDFKit-labs/xctool/archive/0.2.7.pspdfkit1.tar.gz"
-  sha256 "7943fb9d8b83a47bc5c11f5641034b7113b21917facdbcc3a9ff22458eac3296"
-  version "0.2.7.pspdfkit1"
+  url "https://github.com/PSPDFKit-labs/xctool/archive/0.2.7.pspdfkit2.tar.gz"
+  sha256 "c4fc1cf3e978a4fc9be1e001a45e93fd953b126b3bbc6dae9afc8fb4cde0f985"
+  version "0.2.7.pspdfkit2"
   head "https://github.com/PSPDFKit-labs/xctool.git"
 
   depends_on :xcode => "6.0"
