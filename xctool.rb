41f21cd44bc83c4ec082def591a83c6f665c262b
Julian Grosshauser <julian@juliangrosshauser.com>
2015-11-09 13:44:48 +0100

    Explicitly specify version

--- xctool.rb
+++ xctool.rb
@@ -3,6 +3,7 @@ class Xctool < Formula
   homepage "https://github.com/PSPDFKit-labs/xctool"
   url "https://github.com/PSPDFKit-labs/xctool/archive/0.2.7.pspdfkit1.tar.gz"
   sha256 "7943fb9d8b83a47bc5c11f5641034b7113b21917facdbcc3a9ff22458eac3296"
+  version "0.2.7.pspdfkit1"
   head "https://github.com/PSPDFKit-labs/xctool.git"
 
   depends_on :xcode => "6.0"
