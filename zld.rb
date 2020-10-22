b02a5778434903af5800eda55dabaa51e4ad37a1
Peter Steinberger <steipete@gmail.com>
2020-10-22 19:57:03 +0200

    Update zld.rb

--- zld.rb
+++ zld.rb
@@ -1,7 +1,7 @@
 class Zld < Formula
   version "1.2.1"
   desc "A faster version of ld, Apple's linker"
-  url "https://github.com/michaeleisel/zld/releases/download/#{version}/zld.zip"
+  url "https://github.com/michaeleisel/zld/archive/{version}/zld.tar.gz"
   sha256 "fbc8f18687ab779688c2f83333748a6c777d9ba621e4c0c403abb8ee562b07c0"
   homepage "https://github.com/michaeleisel/zld"
 
