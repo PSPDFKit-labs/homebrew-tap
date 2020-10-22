2105a37b80f94e63fdfedfbc30950a0b4d4013a9
Peter Steinberger <steipete@gmail.com>
2020-10-22 19:55:45 +0200

    Create zld.rb

---
+++ zld.rb
@@ -0,0 +1,11 @@
+class Zld < Formula
+  version "1.2.1"
+  desc "A faster version of ld, Apple's linker"
+  url "https://github.com/michaeleisel/zld/releases/download/#{version}/zld.zip"
+  sha256 "fbc8f18687ab779688c2f83333748a6c777d9ba621e4c0c403abb8ee562b07c0"
+  homepage "https://github.com/michaeleisel/zld"
+
+  def install
+    bin.install "zld"
+  end
+end
