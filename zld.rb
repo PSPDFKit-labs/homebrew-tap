882b26ee1a954a7b027287bfce2aaeab988aa711
Peter Steinberger <steipete@gmail.com>
2020-10-22 20:04:42 +0200

    Update zld.rb

--- zld.rb
+++ zld.rb
@@ -1,7 +1,7 @@
 class Zld < Formula
   version "1.2.1"
   desc "A faster version of ld, Apple's linker"
-  url "https://github.com/PSPDFKit-labs/zld/releases/download/#{version}/zld.tar.gz"
+  url "https://github.com/PSPDFKit-labs/zld/releases/download/#{version}/zld.zip"
   sha256 "b9fdd69cc274349625de1fb8df3820d005529cdf8377c47652846b4711b66e4f"
   homepage "https://github.com/michaeleisel/zld"
 
